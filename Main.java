dc7694e9fe1be0c9f8d1f4590258c0fcfbdd185a
Abdullah Ashik <121299454+ash-2732@users.noreply.github.com>
2023-01-13 12:34:16 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main( String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        for( int cs = 1; cs <= test; cs++ ){
+            
+            int n = sc.nextInt();
+            System.out.println(n-1);
+        }
+    }
+}
